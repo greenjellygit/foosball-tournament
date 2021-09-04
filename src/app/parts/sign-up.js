6e2459c981ccfec81bc08384a7e0a8963f746373
GreenJelly <a.krzychu@gmail.com>
2021-09-04 22:46:09 +0200

    [Feature] Add link to tournament table;

--- src/app/parts/sign-up.js
+++ src/app/parts/sign-up.js
@@ -4,7 +4,14 @@ export function SignUp() {
   return (
     <div className="sing-up">
       <div className="sign-up-button">Zgłoś drużynę</div>
-      <div className="button-link">Regulamin rozgrywek</div>
+      <div className="bottom-buttons">
+        <a className="secondary-button"
+           href="https://challonge.com/pl/t12zlvxc"
+           target="_blank">
+          Tabela turniejowa
+        </a>
+        <div className="button-link">Regulamin rozgrywek</div>
+      </div>
     </div>
   );
 }
